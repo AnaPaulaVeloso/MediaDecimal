079e0b1503e19b83b0973d4e1acc197fa7b91ec1
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2021-11-28 13:21:39 -0300

    Concluir a tarefa MediaDecimal

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MediaDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos de números:");
+            decimal quantidade = Convert.ToDecimal(Console.ReadLine());
+
+            decimal NumeroDigitado;
+            int contador = 0;
+
+            decimal soma = 0;
+            decimal maiorNumero = 999M;
+            decimal menorNumero = 999M;
+
+            while (contador < quantidade)
+            {
+                contador += 1;
+                Console.Write($"Número #{contador}: ");
+                NumeroDigitado = Convert.ToDecimal(Console.ReadLine());
+                soma = soma + NumeroDigitado;
+
+                if (contador == 1)
+                {
+                    maiorNumero = NumeroDigitado;
+                    menorNumero = NumeroDigitado;
+                }
+            
+            
+            if (NumeroDigitado > maiorNumero)
+            {
+                maiorNumero = NumeroDigitado;
+            }
+
+            if (NumeroDigitado < menorNumero)
+            {
+                menorNumero = NumeroDigitado;
+
+            }        
+
+            }
+            if (contador > 0)
+            {
+            decimal media = soma / quantidade;
+            
+            Console.WriteLine($"\nSoma ..:{soma:N2}");
+            Console.WriteLine($"Media..:{media:N2}");
+            Console.WriteLine($"Maior..:{maiorNumero:N2}");
+            Console.WriteLine($"Menor..:{menorNumero:N2}");
+            }
+        }
+    }
+}
